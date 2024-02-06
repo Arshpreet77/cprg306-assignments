1f5b470561f922d291a0925209955c60051d3243
Arshpreet77 <131307066+Arshpreet77@users.noreply.github.com>
2024-02-05 20:35:57 -0700

    cprg306-assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+const StudentInfo = () => {
+  return (
+    <div>
+      <p>Arshpreet Singh</p>
+      <Link href="https://github.com/Arshpreet77/cprg306-assignmnets">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+};
+
+export default StudentInfo;
